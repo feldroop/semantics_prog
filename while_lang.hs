13f2858f20cd47527a78af8a0da4110122a9a30c
Felix Droop <felix.droop@fu-berlin.de>
2022-05-09 19:53:03 +0200

    Add u03 (boolean/arithmetic semantics)

--- while_lang.hs
+++ while_lang.hs
@@ -58,6 +58,28 @@ bitValue :: Bit -> Int
 bitValue Zero = 0
 bitValue One = 1
 
+------------------------- While Semantics -------------------------
+arithmeticSemantic :: ArithmeticExp -> (VariableName -> Int) -> Int
+arithmeticSemantic (NumberLiteral n) state = numberSemantics n
+arithmeticSemantic (Variable var) state = state var
+arithmeticSemantic (Addition a1 a2) state = 
+    arithmeticSemantic a1 state + arithmeticSemantic a2 state
+arithmeticSemantic (Multiplication a1 a2) state = 
+    arithmeticSemantic a1 state * arithmeticSemantic a2 state
+arithmeticSemantic (Subtraction a1 a2) state = 
+    arithmeticSemantic a1 state - arithmeticSemantic a2 state
+
+booleanSemantic :: BooleanExp -> (VariableName -> Int) -> Bool
+booleanSemantic TrueLiteral state = True
+booleanSemantic FalseLiteral state = False
+booleanSemantic (EqualTest a1 a2) state = 
+    arithmeticSemantic a1 state == arithmeticSemantic a2 state
+booleanSemantic (SmallerOrEqualTest a1 a2) state = 
+    arithmeticSemantic a1 state <= arithmeticSemantic a2 state
+booleanSemantic (Negation b) state = not (booleanSemantic b state)
+booleanSemantic (And b1 b2) state = 
+    booleanSemantic b1 state && booleanSemantic b2 state
+
 ------------------------- Test Examples -------------------------
 -- -1101_base2 = -13_base10
 aNumber :: Number
@@ -67,8 +89,45 @@ aNumber = Negative (Concat (Concat (Concat (Highest One) One) Zero) One)
 anotherNumber :: Number
 anotherNumber = Positive (Concat (Concat (Highest One) Zero) One)
 
+-- (-13) + (5 * 5) - X
+anArithmeticExpression :: ArithmeticExp
+anArithmeticExpression =
+    Subtraction
+        (Addition
+            (NumberLiteral aNumber)
+            (Multiplication 
+                (NumberLiteral anotherNumber)
+                (NumberLiteral anotherNumber)
+            )
+        )
+        (Variable X)
+
+-- (anArithmeticExpression == Y) AND not false
+aBooleanExpression :: BooleanExp
+aBooleanExpression = 
+    And 
+        (EqualTest
+            anArithmeticExpression
+            (Variable Y)
+        )
+        (Negation FalseLiteral)
+
+-- "compile" the example expressions
+aCompiledArithmeticExp :: (VariableName -> Int ) -> Int
+aCompiledArithmeticExp = arithmeticSemantic anArithmeticExpression
+
+aCompiledBooleanExp :: (VariableName -> Int ) -> Bool
+aCompiledBooleanExp = booleanSemantic aBooleanExpression
+
+-- test state with all variables set to 0
+nullState :: VariableName -> Int 
+nullState _ = 6
+
 ------------------------- Main -------------------------
 main :: IO()
 main = do
     print (numberSemantics aNumber) -- should be -13
     print (numberSemantics anotherNumber) -- should be 5
+
+    print (aCompiledArithmeticExp nullState) -- should be (-13) + (5 * 5) - 0 = 12 
+    print (aCompiledBooleanExp nullState) --should be (12 == 0) AND not false = False
