d00199f5528530c6394551334271eeecda4297bd
Felix Droop <felix.droop@fu-berlin.de>
2022-05-21 11:51:53 +0200

    Add substituion implementations

--- while_lang.hs
+++ while_lang.hs
@@ -50,6 +50,35 @@ booleanSemantic (Negation b) state = not (booleanSemantic b state)
 booleanSemantic (And b1 b2) state = 
     booleanSemantic b1 state && booleanSemantic b2 state
 
+------------------------- Substitution Operators -------------------------
+substitutionArithmetic :: ArithmeticExp -> VariableName -> ArithmeticExp -> ArithmeticExp
+substitutionArithmetic (NumberLiteral n) var a_0 = NumberLiteral n
+substitutionArithmetic (Variable existing_var) var a_0
+    | existing_var == var = a_0
+    | otherwise = Variable existing_var
+substitutionArithmetic (Addition a_1 a_2) var a_0 = 
+    Addition (substitutionArithmetic a_1 var a_0) (substitutionArithmetic a_2 var a_0)
+substitutionArithmetic (Multiplication a_1 a_2) var a_0 = 
+    Multiplication (substitutionArithmetic a_1 var a_0) (substitutionArithmetic a_2 var a_0)
+substitutionArithmetic (Subtraction a_1 a_2) var a_0 = 
+    Subtraction (substitutionArithmetic a_1 var a_0) (substitutionArithmetic a_2 var a_0)
+
+substitutionBoolean :: BooleanExp -> VariableName -> ArithmeticExp -> BooleanExp
+substitutionBoolean TrueLiteral var a_0 = TrueLiteral
+substitutionBoolean FalseLiteral var a_0 = FalseLiteral
+substitutionBoolean (EqualTest a_1 a_2) var a_0 = 
+    EqualTest (substitutionArithmetic a_1 var a_0) (substitutionArithmetic a_2 var a_0)
+substitutionBoolean (SmallerOrEqualTest a_1 a_2) var a_0 = 
+    SmallerOrEqualTest (substitutionArithmetic a_1 var a_0) (substitutionArithmetic a_2 var a_0)
+substitutionBoolean (Negation b_1) var a_0 = Negation (substitutionBoolean b_1 var a_0)
+substitutionBoolean (And b_1 b_2) var a_0 = 
+    And (substitutionBoolean b_1 var a_0) (substitutionBoolean b_2 var a_0)
+
+substitutionState :: State -> VariableName -> Int -> State
+substitutionState state var n param_var
+    | var == param_var = n 
+    | otherwise = state param_var
+
 ------------------------- Test Examples -------------------------
 -- (-13) + (5 * 8) - x
 a :: ArithmeticExp
